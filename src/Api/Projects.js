b4871ec545c407d315c8529fbe01064904d5d35b
KARTIK5799 <thekartiksapkal@gmail.com>
2024-04-10 17:41:46 +0530

    done

--- src/Api/Projects.js
+++ src/Api/Projects.js
@@ -3,21 +3,21 @@ export const projectData = [
     "id": 1,
     "title": "QKart (E-commerce)",
     "description": "QKart is a frontend e-commerce application utilizing context and API integration. It offers a fundamental shopping experience with seamless integration.",
-    "image": "src/assets/qkart.png",
+    "image": "https://images.pexels.com/photos/21357571/pexels-photo-21357571/free-photo-of-website-web-design.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "liveLink": "https://qkart-front-end-kartik.netlify.app/"
   },
   {
     "id": 2,
     "title": "QTrip (Traveling)",
     "description": "QTrip is a straightforward API-based frontend adventure booking site designed to assist users in discovering adventures in various cities.",
-    "image": "src/assets/qtrip.png",
+    "image": "https://images.pexels.com/photos/21357573/pexels-photo-21357573/free-photo-of-website-web-design.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "liveLink": "https://qtrip-1979.netlify.app/"
   },
   {
     "id": 3,
     "title": "XBoard (News)",
     "description": "XBoard is a news portal presenting updated news in carousel and dropdown formats, relying entirely on API data for content delivery.",
-    "image": "src/assets/xnews.png",
+    "image": "https://images.pexels.com/photos/21357572/pexels-photo-21357572/free-photo-of-website-web-design.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "liveLink": "https://newsportal-1997.netlify.app/"
   },
   {
@@ -31,21 +31,21 @@ export const projectData = [
     "id": 5,
     "title": "QKart (E-commerce)",
     "description": "QKart is a frontend e-commerce application utilizing context and API integration. It offers a fundamental shopping experience with seamless integration.",
-    "image": "src/assets/qkart.png",
+    "image": "https://images.pexels.com/photos/21357571/pexels-photo-21357571/free-photo-of-website-web-design.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "liveLink": "https://qkart-front-end-kartik.netlify.app/"
   },
   {
     "id": 6,
     "title": "QTrip (Traveling)",
     "description": "QTrip is a straightforward API-based frontend adventure booking site designed to assist users in discovering adventures in various cities.",
-    "image": "src/assets/qtrip.png",
+    "image": "https://images.pexels.com/photos/21357573/pexels-photo-21357573/free-photo-of-website-web-design.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "liveLink": "https://qtrip-1979.netlify.app/"
   },
   {
     "id": 7,
     "title": "XBoard (News)",
     "description": "XBoard is a news portal presenting updated news in carousel and dropdown formats, relying entirely on API data for content delivery.",
-    "image": "https://images.pexels.com/photos/20927624/pexels-photo-20927624/free-photo-of-a-woman-sitting-on-the-floor-with-her-legs-crossed.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "image": "https://images.pexels.com/photos/21357572/pexels-photo-21357572/free-photo-of-website-web-design.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "liveLink": "https://newsportal-1997.netlify.app/"
   },
 
